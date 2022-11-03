e000566227e6d53290512fe5d2b32662b056fab5
andy <D:\users\bushuev_an\The Bat!>
2022-11-03 23:38:43 +0700

    получение заголовка и опиания

--- links_v1.py
+++ links_v1.py
@@ -19,24 +19,14 @@ link = pyperclip.paste()
 
 def link_in():
 	if type(link) == str and link.startswith('http'):
-		#res = requests.get(link, headers)
-		res = Zapros().res
-		ppp = Page_links(res)
-		Main_win(window)
-		
-		return ppp
+		res = requests.get(link, headers)
+		eSoup = bs4.BeautifulSoup(res.text, 'lxml')
+		title = eSoup.title.text
+		Main_win(window, title)
+		print(eSoup)
 	else:
 		messagebox.showerror('Это не ссылка!', 'Данные не являются ссылкой')
 
-class Zapros:
-	def __init__(self):
-		self.res = requests.get(link, headers)
-
-class Page_links:
-	def __init__(self, res):
-		self.eSoup = bs4.BeautifulSoup(res.text, 'lxml')
-		self.title = self.eSoup.title.txt
-		print(self.res)
 
 """
 #rubrica = 'Ссылки'
@@ -148,14 +138,15 @@ window.mainloop()
 """
 
 class Main_win:
-	def __init__(self, master):
+	def __init__(self, master, title):
+		self.title = title
 		self.master = master
 		self.master.title('Описание ссылки')
 		self.lbl_url = LabelFrame(text='URL')
 		self.lbl_url.pack(anchor=W)
 		self.mes_url = Message(self.lbl_url, text=link, width=500)
 		self.mes_url.pack()
-		self.mes_title = Message(text='title', width=500)
+		self.mes_title = Message(text='self.title', width=500)
 		self.mes_title.pack(anchor=W)
 		self.txt_opis = Text( width=60, height=10)
 		self.txt_opis.insert('1.0', 'Здесь будет описание ссылки')
@@ -171,8 +162,8 @@ class Main_win:
 		
 	
 window = Tk()
-ppp = link_in()
-print(ppp.title)
+title = link_in()
+#print(ppp.title)
 #Main_win(window)
 
 
