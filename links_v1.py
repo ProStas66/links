dc7ba5c3078fa43d29d8f05477141945db119759
andy <D:\users\bushuev_an\The Bat!>
2022-11-25 19:30:22 +0700

    Запись данных в html страницу

--- links_v1.py
+++ links_v1.py
@@ -16,7 +16,7 @@ wd, filename = os.path.split(wf)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'}
 item = []
 link = pyperclip.paste()
-link = 'https://sky.pro/head'
+#link = 'https://sky.pro/head'
 
 def link_file():
 	if os.path.isfile('links.html'):
@@ -30,16 +30,43 @@ def link_in():
 		res = requests.get(link, headers=headers)
 		eSoup = bs4.BeautifulSoup(res.text, 'lxml')
 		title = eSoup.title.text
+		
 		metatags = eSoup.find('meta', attrs={'name':'description'})
 		if metatags:
 			opisanie = metatags.get('content')
 		else:
 			opisanie = 'Описание отсутствует'
 		Main_win(window, title, opisanie)
-		print(link_file())
+		#links_html(title, opisanie)
 	else:
 		messagebox.showerror('Это не ссылка!', 'Данные не являются ссылкой')
 
+def links_html(title, opisanie):
+	with open(link_file(), 'r', encoding='utf-8') as f_in:
+		contents = f_in.read()
+	soup = bs4.BeautifulSoup(contents, 'lxml')
+	
+	li_tag = soup.new_tag('li')
+	a_tag = soup.new_tag('a', href=link)
+	a_tag.string = title
+	a_tag.string.wrap(soup.new_tag('H3'))
+	li_tag.append(a_tag)
+	li_tag.append(opisanie)
+	soup.ul.append(li_tag)
+	
+	list_li = soup('li')
+	list_li = sorted(list_li, key = lambda elem: elem.a.get('href'))
+	ul_tag = soup.find('ul')
+	print(ul_tag)
+	ul_tag.clear()
+	for elem in list_li:
+		ul_tag.append(elem)
+	print('clean:', ul_tag)
+	
+	soup = soup.prettify()
+	with open ('links.html', 'w', encoding='utf-8') as f_out:
+		f_out.write(soup)
+	print(soup)
 
 """
 #rubrica = 'Ссылки'
@@ -162,17 +189,47 @@ class Main_win:
 		self.mes_url.pack()
 		self.mes_title = Message(text=self.title, width=500)
 		self.mes_title.pack(anchor=W)
+		self.txt_title = Entry(width=60)
+		self.txt_title.insert(0, self.title)
+		self.txt_title.pack()
 		self.txt_opis = Text( width=60, height=10)
 		self.txt_opis.insert('1.0', self.opisanie)
 		self.txt_opis.pack()
-		self.btn_ok = Button(text='OK')
+		self.btn_ok = Button(text='OK', command=self.make_html)
 		self.btn_ok.pack(side=RIGHT)
 		self.btn_out = Button(text='Cancel', command=self.killwin)
 		self.btn_out.pack(side=RIGHT)
 		self.master.mainloop()
 		
+	def make_html(self):
+		with open(link_file(), 'r', encoding='utf-8') as f_in:
+			contents = f_in.read()
+		soup = bs4.BeautifulSoup(contents, 'lxml')
+		
+		li_tag = soup.new_tag('li')
+		a_tag = soup.new_tag('a', href=link)
+		a_tag.string = self.title
+		a_tag.string.wrap(soup.new_tag('H3'))
+		li_tag.append(a_tag)
+		li_tag.append(self.txt_opis.get(1.0, END))
+		soup.ul.append(li_tag)
+		
+		list_li = soup('li')
+		list_li = sorted(list_li, key = lambda elem: elem.a.get('href'))
+		ul_tag = soup.find('ul')
+		ul_tag.clear()
+		for elem in list_li:
+			ul_tag.append(elem)
+		soup = soup.prettify()
+		with open ('links.html', 'w', encoding='utf-8') as f_out:
+			f_out.write(soup)
+		self.killwin()
+				
+	
 	def killwin(self):
 		window.destroy()
+	
+	
 		
 	
 window = Tk()
